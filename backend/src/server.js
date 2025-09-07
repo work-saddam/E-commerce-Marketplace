6f2b6b9fb4e4a6b8b4d68bd64fa49606d5406e21
Saddam Hussein <work.saddam786@gmail.com>
2025-09-07 17:52:58 +0530

    add auth routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/database");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routers/authRouter");
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 connectDB();
 
-app.use("/", (req, res) => {
-  res.send("Home");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
