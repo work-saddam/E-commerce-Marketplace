0a1c59f02984656d93666846c85fd90ff18a787d
Saddam Hussein <work.saddam786@gmail.com>
2025-09-08 09:24:23 +0530

    add validation

--- backend/src/utils/validation.js
+++ backend/src/utils/validation.js
@@ -11,8 +11,33 @@ const validateUserRegisterData = (req) => {
   } else if (!validator.isStrongPassword(password)) {
     return { status: 400, message: "Password not Strong" };
   } else if (!validator.isMobilePhone(phone)) {
-    return { status: 400, message: "Invalid Mobile number" };
+    return { status: 400, message: "Invalid Phone number" };
   }
 };
 
-module.exports = { validateUserRegisterData };
+const validateUserEditProfileData = (req) => {
+  const { name } = req.body;
+  if (name.length < 3) {
+    return { status: 400, message: "Invalid Name" };
+  }
+};
+
+const validateAddressData = (req) => {
+  const { name, phone, street, city, state, pincode, country } = req.body;
+
+  if (!name || !phone || !street || !city || !state || !pincode || !country) {
+    return { status: 400, message: "All fields is required" };
+  } else if (name.length < 3) {
+    return { status: 400, message: "Invalid Name" };
+  } else if (!validator.isMobilePhone(phone)) {
+    return { status: 400, message: "Invalid Phone number" };
+  } else if (pincode.length !== 6) {
+    return { status: 400, message: "Invalid Pincode" };
+  }
+};
+
+module.exports = {
+  validateUserRegisterData,
+  validateUserEditProfileData,
+  validateAddressData,
+};
