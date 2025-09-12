589957a1a59e1f3dd622c71c76df67f500a3c1d2
Saddam Hussein <work.saddam786@gmail.com>
2025-09-12 08:38:28 +0530

    add product model

---
+++ backend/src/models/product.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seller",
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    imageURL: { type: String },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+productSchema.pre("save", function (next) {
+  if (this.isModified("title")) {
+    const baseSlug = slugify(this.title, { lower: true, strict: true });
+    this.slug = `${baseSlug}-${this._id}`;
+  }
+  next();
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
