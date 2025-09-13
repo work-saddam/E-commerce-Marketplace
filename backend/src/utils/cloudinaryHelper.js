f405686171fba8aa5d3c8875dbdde229140fc862
Saddam Hussein <work.saddam786@gmail.com>
2025-09-13 11:49:14 +0530

    add cloudinary helper function

---
+++ backend/src/utils/cloudinaryHelper.js
@@ -0,0 +1,16 @@
+const cloudinary = require("../config/cloudinary");
+
+const uploadToCloudinary = (fileBuffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder },
+      (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      }
+    );
+    stream.end(fileBuffer);
+  });
+};
+
+module.exports = { uploadToCloudinary };
