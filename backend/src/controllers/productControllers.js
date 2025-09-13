11beabab800c3e9394f9159adebf186c8676cd4b
Saddam Hussein <work.saddam786@gmail.com>
2025-09-13 11:51:31 +0530

    add product controller to add product

---
+++ backend/src/controllers/productControllers.js
@@ -0,0 +1,51 @@
+const Product = require("../models/product");
+const { uploadToCloudinary } = require("../utils/cloudinaryHelper");
+const { validateProductData } = require("../utils/validation");
+
+const addProduct = async (req, res) => {
+  try {
+    const error = validateProductData(req);
+    if (error) {
+      return res.status(error.status).json({ message: error.message });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "Product image is required!" });
+    }
+
+    let imageURL, imagePublicId;
+
+    try {
+      const uploadRes = await uploadToCloudinary(req.file.buffer, "products");
+      imageURL = uploadRes.secure_url;
+      imagePublicId = uploadRes.public_id;
+    } catch (uploadErr) {
+      return res.status(500).json({
+        message: "Image upload failed!",
+        error: uploadErr.message,
+      });
+    }
+
+    const { title, description, price, category, stock } = req.body;
+
+    const product = await Product.create({
+      title,
+      description,
+      price: Number(price),
+      category,
+      seller: req.user.id,
+      stock: Number(stock),
+      image: { url: imageURL, public_id: imagePublicId },
+    });
+
+    res
+      .status(201)
+      .json({ message: "Product added successfully!", data: product });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error adding product!", error: error.message });
+  }
+};
+
+module.exports = { addProduct };
--- backend/src/routers/sellerRouter.js
+++ backend/src/routers/sellerRouter.js
@@ -5,10 +5,21 @@ const {
   getSellerProfile,
 } = require("../controllers/sellerController");
 const { userAuth } = require("../middlewares/authMiddleware");
+const { addProduct } = require("../controllers/productControllers");
+const checkSellerStatus = require("../middlewares/checkSellerStatus");
+const upload = require("../middlewares/multer");
 const router = express.Router();
 
 router.post("/register", sellerRegister);
 router.post("/login", sellerLogin);
 router.get("/profile", userAuth, getSellerProfile);
+
+router.post(
+  "/products",
+  userAuth,
+  checkSellerStatus,
+  upload.single("productImage"),
+  addProduct
+);
 
 module.exports = router;
