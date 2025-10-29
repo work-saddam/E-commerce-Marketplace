e82b1c98826d291a240eaea4bca2671d04521a83
Saddam Hussein <work.saddam786@gmail.com>
2025-10-29 08:03:09 +0530

    feat: login

--- seller/src/App.jsx
+++ seller/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
+import { Login } from "./pages/Login";
 import Body from "./components/Body";
 
 function App() {
--- seller/src/pages/Login.jsx
+++ seller/src/pages/Login.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { BASE_URL } from "@/utils/constant";
+import axios from "axios";
+import { useState } from "react";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+export function Login() {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default Login;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    console.log("handlesubmit");
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/api/seller/login`,
+        { identifier, password },
+        { withCredentials: true }
+      );
+      console.log(res?.data?.data);
+    } catch (error) {
+      setError(
+        error?.response?.data?.message ||
+          "Something went wrong. Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-50 px-4">
+      <Card className="w-full max-w-sm ">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email or phone number below to login
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">Sign Up</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email/Mobile</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  value={identifier}
+                  onChange={(e) => setIdentifier(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            {error && (
+              <p className="text-red-500 text-sm text-center pt-2">{error}</p>
+            )}
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button
+            type="submit"
+            className="w-full"
+            onClick={handleSubmit}
+            disabled={loading}
+          >
+            {loading ? "Logging in..." : "Login"}
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
