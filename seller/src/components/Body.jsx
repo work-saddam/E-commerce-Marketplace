f7f59fefaab9b254df05ad1d511d5753e911e142
Saddam Hussein <work.saddam786@gmail.com>
2025-11-19 08:11:50 +0530

    add imports

--- seller/src/components/Body.jsx
+++ seller/src/components/Body.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { BASE_URL } from "@/utils/constant";
+import { addUser, removeUser, setLoading } from "@/store/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
   const { user } = useSelector((store) => store.user);
 
   const fetchUser = async () => {
