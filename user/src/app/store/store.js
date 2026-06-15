df5459162a687b8a99a9745bd933bb46e1c24766
Saddam Hussein <work.saddam786@gmail.com>
2026-06-15 12:00:21 +0530

    setup redux

---
+++ user/src/app/store/store.js
@@ -0,0 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import rootReducer from "./rootReducer";
+
+// Workaround for Vite ESM / CJS resolution mismatch with redux-persist
+const persistStorage = storage?.default || storage;
+
+const persistConfig = {
+  key: "root",
+  storage: persistStorage,
+  whitelist: ["user"], // Persist only user/auth state for now
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
